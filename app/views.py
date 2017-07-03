d7b81b402418a489ecd0ce49f491dc16d50f9373
Daniel Wangai <daniel.maina@andela.com>
2017-07-03 10:31:11 +0300

    [Feature] Define parameters for login.

---
+++ app/views.py
@@ -0,0 +1,29 @@
+"""
+To handle API CRUD operations.
+"""
+from flask_restful import Resource, reqparse
+
+
+class UserLogin(Resource):
+    """
+    To handle user login.
+    """
+    def __init__(self):
+        """
+        Define parameters for login endpoint.
+        """
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("username", type=str,
+                                   help="Username required",
+                                   required=True
+                                   )
+        self.reqparse.add_argument("password", type=str,
+                                   help="Password required",
+                                   required=True
+                                   )
+
+    def post(self):
+        """
+        To handle login
+        """
+        pass
